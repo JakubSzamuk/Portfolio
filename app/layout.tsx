8e0b7384589bb44c19e4bf44a728a9ecfdf83c06
Jakub Szamuk <lavaproofjakub1@gmail.com>
2023-08-16 15:34:12 +0000

    favicon works

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,9 +23,9 @@ export default function RootLayout({
         <meta property="og:title" content="Jakub Szamuk's Portfolio" key="title" />
         <meta property='og:description' content='Freelance Full-Stack web developer, website created by Jakub Szamuk' />
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest"></link>
         <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Roboto+Slab:wght@100;400&display=swap" rel="stylesheet" /> 
       </head>
